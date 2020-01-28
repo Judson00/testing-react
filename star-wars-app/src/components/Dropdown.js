5a51750202e45e50f39f0b5af6b794abf0e1d528
Robert J. Mitchell <robert.judson@outlook.com>
2020-01-27 19:02:55 -0500

    Dropdown finished

--- star-wars-app/src/components/Dropdown.js
+++ star-wars-app/src/components/Dropdown.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { useDispatch } from 'react-redux';
 
 const Menu = (props) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const toggle = () => setDropdownOpen(prevState => !prevState);
+
+  const dispatch = useDispatch();
 
   return (
     <Dropdown isOpen={dropdownOpen} toggle={toggle}>
@@ -14,12 +16,10 @@ const Menu = (props) => {
         </DropdownToggle>
       <DropdownMenu>
         <DropdownItem header>Header</DropdownItem>
-        <DropdownItem>Some Action</DropdownItem>
-        <DropdownItem disabled>Action (disabled)</DropdownItem>
-        <DropdownItem divider />
-        <DropdownItem>Foo Action</DropdownItem>
-        <DropdownItem>Bar Action</DropdownItem>
-        <DropdownItem>Quo Action</DropdownItem>
+        <DropdownItem onClick={() => dispatch({ type: "PLANETS" })}>Planets</DropdownItem>
+        <DropdownItem onClick={() => dispatch({ type: "STARSHIPS" })}>Starships</DropdownItem>
+        <DropdownItem onClick={() => dispatch({ type: "VEHICLES" })}>Vehicles</DropdownItem>
+        <DropdownItem onClick={() => dispatch({ type: "SPECIES" })}>Species</DropdownItem>
       </DropdownMenu>
     </Dropdown>
   );
